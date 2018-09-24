8cf3eeb95a8ae9d5aaf181aaab17dcb202924d46
xArtDTx <35765041+xArtDTx@users.noreply.github.com>
2018-09-24 10:14:02 +0300

    Сделал первое задание.
    Однако у меня студия ругается да и не получается вывести данные в датагрид(опять). мб трабла локальная толкьо у меня на машине. просто странно Роман у вас в примере все работало нормально а у меня как всегда. привязку в Xaml сделал иденттчной вашему примеру. странно то что у вас все нормально работает а у меня вывода данных в дата гридл нет. самое забавноев том, что в таблицу данные добавились(it's magic XD)

---
+++ EfHwApp/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace EfHwApp
+{
+    
+    public partial class MainWindow : Window
+    {
+        private readonly DataBaseContainer _financialAccounting = new DataBaseContainer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = _financialAccounting;
+        }
+
+        private void MainWindow_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            _financialAccounting.FinancialAccountingSet.Load();
+        }
+
+        private void AddButton_OnClick(object sender, RoutedEventArgs e)
+        {
+           
+            var financialAccounting = new FinancialAccounting
+            {
+                Profit = ProfitTxt.Text,
+                Costs = CostsTxt.Text
+            };
+
+            _financialAccounting.FinancialAccountingSet.Add(financialAccounting);
+            _financialAccounting.SaveChanges();
+        }
+    }
+}
